da803383fd6a220e4b399b0416c0eaf7c08da4d2
Sergey <gck.sergey@gmail.com>
2018-09-17 15:04:33 +0300

    gotovo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+ equation = equation.split(' ');
+ let x1, x2;
+ let a = equation[0];
+ let b = equation[3]+equation[4];
+ let c = equation[7]+equation[8];
+ let D = b*b - 4*a*c;
+ x1 = Math.round((-b + Math.sqrt(D))/2/a);
+ x2 = Math.round((-b - Math.sqrt(D))/2/a);
+ let array = [];
+ array.push(x1, x2);
+ return array.sort((a,b)=>a-b);  
 }
